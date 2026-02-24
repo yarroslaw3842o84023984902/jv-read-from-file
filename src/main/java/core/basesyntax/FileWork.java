3990fd91a4738cb34c44bcba86d8e0bb9033a30b
Yaroslav Petruk <yarroslawjob@gmail.com>
2026-02-24 15:55:01 +0100

    homSol

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,40 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        List<String> lines;
+        try {
+            lines = Files.readAllLines(Path.of(fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        List<String> result = new ArrayList<>();
+        for (String el : lines) {
+            String[] words = el.split(" ");
+            for (String elWords : words) {
+                String wordsToLowerCase = elWords.toLowerCase();
+                char[] charWordsToLowerCase = wordsToLowerCase.toCharArray();
+                StringBuilder stringBuilder = new StringBuilder();
+                String clearWord = "";
+                for (char ch : charWordsToLowerCase) {
+                    if (Character.isLetter(ch)) {
+                        clearWord = stringBuilder.append(ch).toString();
+                    }
+                }
+                if (clearWord.startsWith("w")) {
+                    result.add(clearWord);
+                }
+            }
+        }
+        String[] arr = result.toArray(new String[0]);
+        Arrays.sort(arr);
+        return arr;
     }
 }
---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,8 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+            FileWork fileWork = new FileWork();
+            fileWork.readFromFile("test2");
+    }
+}
